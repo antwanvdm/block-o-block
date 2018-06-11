f0bdc703f0f77c6a56d3961c9d57809287cb4fc3
antwan <antwanvdm@gmail.com>
2018-06-11 12:12:53 +0200

    documentation jsDOC

--- dev/helpers/utils.ts
+++ dev/helpers/utils.ts
@@ -1,13 +1,26 @@
 export default class Utils {
+    /**
+     * @returns {string}
+     */
     static getRandomColor(): string {
         let o = Math.round, r = Math.random, s = 255;
         return `rgba(${o(r() * s)},${o(r() * s)},${o(r() * s)},1)`;
     }
 
+    /**
+     * @param {number} min
+     * @param {number} max
+     * @returns {number}
+     */
     static getRandomInt(min: number, max: number): number {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
+    /**
+     * @param {ClientRect} a
+     * @param {ClientRect} b
+     * @returns {boolean}
+     */
     static checkCollision(a: ClientRect, b: ClientRect): boolean {
         return (a.left <= b.right &&
             b.left <= a.right &&
--- dev/helpers/windoweventhandler.ts
+++ dev/helpers/windoweventhandler.ts
@@ -1,11 +1,22 @@
 export default class WindowEventHandler {
     private static functionMap: { [k: string]: any } = {};
 
+    /**
+     * Add listener to window with name spacing
+     *
+     * @param {string} event
+     * @param func
+     */
     public static addEventListener(event: string, func: any): void {
         WindowEventHandler.functionMap[event] = func;
         window.addEventListener(event.split('.')[0], WindowEventHandler.functionMap[event]);
     }
 
+    /**
+     * Remove listener from window with name spacing
+     *
+     * @param {string} event
+     */
     public static removeEventListener(event: string): void {
         window.removeEventListener(event.split('.')[0], WindowEventHandler.functionMap[event]);
         delete WindowEventHandler.functionMap[event];
